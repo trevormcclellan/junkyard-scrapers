41868b350b3634a8632f678fc373cdd1438d3fbe
Trevor McClellan <trevormcclellan@gmail.com>
2024-07-31 20:16:14 -0600

    Fix UTPAP scraper for new website design

--- utpap/main.py
+++ utpap/main.py
@@ -41,21 +41,26 @@ def delete_old_records(existing_cars, latest_cars):
             collection.delete_one({"stock_num": car['stock_num']})
             print(f"{str(datetime.now())} - Deleted record with stock_num: {car['stock_num']}")
 
-url = "https://utpap.com/wp-content/themes/enterprise/inventory_search_files/search-inventory_orem.php?make=MERCEDES-BENZ&model="
+url = "https://utpap.com/orem-inventory"
 
 payload = {}
 headers = {
-    'Referer': 'https://utpap.com/wp-content/themes/enterprise/inventory_search_files/search-inventory_orem.php',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
-    'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9',
+    'sec-ch-ua': '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
     'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"'
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
 }
 
 response = requests.get(url, headers=headers, data=payload)
 soup = BeautifulSoup(response.text, 'html.parser')
-table = soup.find('table', {'class': 'resultsTable', 'id': 'cars-table'})
+table = soup.find('table', {'id': 'vehicles'})
 
 cars_of_interest = []
 
@@ -73,30 +78,34 @@ if table:
         if col_data:
             try:
                 year = int(col_data[0])
+                make = col_data[1].upper()
                 model = col_data[2].upper()
-                stock_num = col_data[3]
-                row = col_data[4]
-                date = col_data[6]
-                image = f"https://utpap.com/wp-content/themes/enterprise/inventory-photos-orem/{stock_num}.jpeg"
+                color = col_data[3]
+                stock_num = col_data[5]
+                row = col_data[6]
+                date = col_data[7]
+                image = f"https://utpap.com/Orem-inventory-photos/{stock_num}.jpeg"
 
                 car_data = {
                     "year": year,
                     "model": model,
+                    "color": color,
                     "stock_num": stock_num,
                     "row": row,
                     "date": date,
                     "image": image
                 }
 
-                if (year >= 1976 and year <= 1985) or (year >= 1996 and year <= 2002 and model == "E-CLASS"):
-                    cars_of_interest.append(car_data)
-                    # Check if the car is already in the database
-                    existing_car = collection.find_one({"stock_num": stock_num})
-                    if existing_car is None:
-                        # Send the notification
-                        send_to_home_assistant(car_data)
-                        # Add the car to the database
-                        collection.insert_one(car_data)
+                if "MERCEDES" in make:
+                    if (year >= 1976 and year <= 1985) or (year >= 1996 and year <= 2002 and model == "E-CLASS"):
+                        cars_of_interest.append(car_data)
+                        # Check if the car is already in the database
+                        existing_car = collection.find_one({"stock_num": stock_num})
+                        if existing_car is None:
+                            # Send the notification
+                            send_to_home_assistant(car_data)
+                            # Add the car to the database
+                            collection.insert_one(car_data)
             except ValueError:
                 # Handle the case where conversion to int fails (e.g., year is not a number)
                 print(f"{str(datetime.now())} - Skipping row with invalid data: {col_data}")
